590262ea4f8b2ade88625fe251f25b406ee2d4bc
RecoloredPoyo4 <82355015+RecoloredPoyo4@users.noreply.github.com>
2022-05-13 08:50:01 -0400

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2621,7 +2621,6 @@ class PlayState extends MusicBeatState
 
 				if (SONG.validScore)
 				{
-					NGio.unlockMedal(60961);
 					Highscore.saveWeekScore(storyWeek, campaignScore, storyDifficulty);
 				}
 

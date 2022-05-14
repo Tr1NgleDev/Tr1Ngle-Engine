e9c4cc7a9cbfa3aed6e9450bdbc3fb7b3fffc74c
RecoloredPoyo4 <82355015+RecoloredPoyo4@users.noreply.github.com>
2022-05-13 21:43:47 -0400

    update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -226,29 +226,29 @@ class StoryMenuState extends MusicBeatState
 		{
 			if (!selectedWeek)
 			{
-				if (controls.UP)
+				if (controls.UP_P)
 				{
 					changeWeek(-1);
 				}
 
-				if (controls.DOWN)
+				if (controls.DOWN_P)
 				{
 					changeWeek(1);
 				}
 
-				if (controls.RIGHT)
+				if (controls.RIGHT_P)
 					rightArrow.animation.play('press')
 				else
 					rightArrow.animation.play('idle');
 
-				if (controls.LEFT)
+				if (controls.LEFT_P)
 					leftArrow.animation.play('press');
 				else
 					leftArrow.animation.play('idle');
 
-				if (controls.RIGHT)
+				if (controls.RIGHT_P)
 					changeDifficulty(1);
-				if (controls.LEFT)
+				if (controls.LEFT_P)
 					changeDifficulty(-1);
 			}
 
